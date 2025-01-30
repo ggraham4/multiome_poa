1d44949986fddca28ae93f5c4069e0bd39d14735
ggraham4 <ggraham4@illinois.edu>
2025-01-30 14:54:11 -0500

    Looked at some differentially expressed eigengenes, but I think I did it wrong

--- WGCNA/WGCNA POA Cells 01292025.R
+++ WGCNA/WGCNA POA Cells 01292025.R
@@ -42,6 +42,8 @@ mean_expression_cluster_data<- readRDS('Functions/mean_expression_cluster_data.r
 prop_deg_function.rds<- readRDS('Functions/DEG_functions/prop_deg_function.rds')
 define_behavior_degs<- readRDS('Functions/define_behavior_degs')
 clown_go<- readRDS('Functions/clown_go')
+define_degs<- readRDS('Functions/define_degs')
+
 library(hdWGCNA)
 library(WGCNA)
 
@@ -225,6 +227,7 @@ module_go_function('green')
 module_go_function('magenta')
 module_go_function('purple')
 module_go_function('blue')
+module_go_function('pink')
 
 
 modules%>%
@@ -279,11 +282,203 @@ ModuleUMAPPlot(
     return_graph = F
 )
 #saveRDS(obj, file='~/Desktop/snRNA-seq R Files 122524/hdWGCNA_object_putative_poa_cells.rds')
+obj <- readRDS(file='~/Desktop/snRNA-seq R Files 122524/hdWGCNA_object_putative_poa_cells.rds')
+
+#### Testing for DEG Enrichment ####
+
+#read in and coalesce DEG list
+together_data <- data.frame()
+for(i in 0:31){
+data <- read.csv(paste0('DEG Outputs/122324 Neg Bin with Doms/cluster_',i,'.csv')
+)
+data <- define_degs(data)
+together_data <- rbind(data, together_data)
+  
+  }
+degs <- together_data$gene[!is.na(together_data$class)]
+
+##test which modules are enriched for DEGs
+MEs <- GetMEs(obj, harmonized=TRUE)
+modules <- GetModules(obj)
+
+modules_filtered <- subset(modules, color != 'grey')
+modules_summarized <- modules_filtered%>%
+  group_by(module)%>%
+  summarize(genes_in_module = n(),
+            significant_genes_in_module = sum(gene_name %in% degs))
+
+modules_summarized$insignificant_genes_in_module <- modules_summarized$genes_in_module - modules_summarized$significant_genes_in_module
+module_matrix <- as.matrix(modules_summarized[,3:4])
+
+###Fisher's exact test
+fish_test <- fisher.test(module_matrix,simulate.p.value=TRUE)
+
+expected <- outer(rowSums(module_matrix), colSums(module_matrix)) / sum(module_matrix)
+residuals <- (module_matrix - expected) / sqrt(expected * (1 - rowSums(module_matrix) / sum(module_matrix)) * (1 - colSums(module_matrix) / sum(module_matrix)))
+alpha <- 0.05  # Significance level
+critical_value <- qnorm(1 - alpha / 2)  # Two-tailed test
+
+significant_cells <- abs(residuals) > critical_value
+print(significant_cells)
+rownames(significant_cells) = modules_summarized$module
+
+sig_cells_data <- as.data.frame(significant_cells)
+sig_cells_data$module = rownames(sig_cells_data)
+colnames(sig_cells_data) = c('sig_genes','insig_genes','module')
+
+plot_data <- modules_summarized%>%
+  right_join(sig_cells_data, by ='module')
+plot_data$prop_signif = plot_data$significant_genes_in_module/plot_data$genes_in_module
+
+ggplot(plot_data, aes(x = fct_reorder(module, prop_signif, .desc =T), y = prop_signif))+
+  geom_bar(stat = 'identity')+
+  geom_text(aes(label =sig_genes))+
+  geom_hline(yintercept = mean(plot_data$prop_signif))
 
 
+#### Lets look at Pink ####
+plot_list <- ModuleFeaturePlot(
+  obj,
+  reduction = 'harmony_wnn.umap',
+  features='MEs', 
+  order=TRUE 
+)
+plot_list[["pink"]]+DimPlot(obj)
+#ok the DimPlot umap and metacel umap is different
+
+DotPlot(obj, features=mods, group.by = 'harmony.wnn_res0.4_clusters')+
+  coord_flip()
+#pretty weak in 19 tbh, what is special about 19?
+
+modules$gene_name[modules$color=='pink']
+
+colnames(obj@meta.data)
+##### is pink differentially expressed by sex? ###
+
+hMEs <- GetMEs(obj, harmonized=FALSE)
+#i think the way to do this is by hMEs
+
+model_data<- data.frame(
+  pink= hMEs$pink,
+  individual = obj@meta.data$individual, 
+  Status = obj@meta.data$Status,
+  cluster = obj@meta.data$harmony.wnn_res0.4_clusters)
+
+ggplot(model_data, aes(x = cluster, y = pink))+
+  geom_point(aes(size = pink))
+
+hist(model_data$pink)
+
+model_data_mean <- model_data%>%
+  group_by(individual, Status, cluster)%>%
+  summarize(mean_pink = mean(pink))
+
+###first, lets see across all clusters
+model_all_pink <- lmer(mean_pink~Status+(1|individual),
+                       data = model_data_mean)
+summary(model_all_pink)
+car::Anova(model_all_pink, type = 'III')
+### nope
+
+###what about just 19
+model_19_pink <- lm(mean_pink~Status,
+                       data = subset(model_data_mean, cluster ==19))
+summary(model_19_pink)
+anova(model_19_pink, test ='Chisq')
+
+#ok give me all of them
+model_outs <- data.frame()
+for(i in unique(obj@meta.data$harmony.wnn_res0.4_clusters)){
+  
+  model_i_pink <- lm(mean_pink~Status,
+                       data = subset(model_data_mean, cluster ==i))
+  av <- as.data.frame(anova(model_i_pink, test ='Chisq'))
+  
+  new_data <- data.frame(cluster = i,
+                         anova_p = av$`Pr(>F)`[1])
+  model_outs <- rbind(model_outs, new_data)
+  
+}
+#nothin
+
+###what does it look like if you dont do mean
+model_19_cells <- lmer(pink~Status+(1|individual),
+                       data = subset(model_data, cluster ==19))
+car::Anova(model_19_cells, type = 'III')
+#still nothing, actually a similar p value too so theres probably nothing
+
+DotPlot(subset(obj, harmony.wnn_res0.4_clusters==19), features=mods, group.by = 'harmony.wnn_res0.4_clusters', split.by = 'Status', cols = P40)+
+  coord_flip()
 
 
+####lets do the same for brown ####
+brown_genes <- modules$gene_name[modules$color=='brown']
 
+brown_model_data<- data.frame(
+  brown= hMEs$brown,
+  individual = obj@meta.data$individual, 
+  Status = obj@meta.data$Status,
+  cluster = obj@meta.data$harmony.wnn_res0.4_clusters)
+
+model_19_cells_brown <- lmer(brown~Status+(1|individual),
+                       data = subset(brown_model_data, cluster ==19))
+summary(model_19_cells_brown)
+car::Anova(model_19_cells_brown, type = 'III')
+#nothin
+
+
+#### all combinations ####
+all_combs <- data.frame()
+for(color in unique(modules$color)){
+  
+  color_model_data<- data.frame(
+  color= hMEs[[color]],
+  individual = obj@meta.data$individual, 
+  Status = obj@meta.data$Status,
+  cluster = obj@meta.data$harmony.wnn_res0.4_clusters)
+
+  for(b in unique(obj@meta.data$harmony.wnn_res0.4_clusters)){
+    
+    cluster_color_model_data <- subset(color_model_data, cluster == b)
+    
+    model_cells <- lmer(color~Status+(1|individual),
+                       data = cluster_color_model_data)
+
+av <- as.data.frame(car::Anova(model_cells, type = 'III'))
+
+  new_dat <- data.frame(cluster = b,
+                      color = color,
+                      p.value = av$`Pr(>Chisq)`[2])
+all_combs <- rbind(all_combs, new_dat)
+
+  }
+
+  }
+
+all_combs$issignif <- ifelse(all_combs$p.value<0.05, '*', NA)
+#not applying a fdr rn cause this is just exploratory
+
+#### Purple ####
+purple_genes <- modules$gene_name[modules$color=='purple']
+
+purple_model_data<- data.frame(
+  purple= hMEs$purple,
+  individual = obj@meta.data$individual, 
+  Status = obj@meta.data$Status,
+  cluster = obj@meta.data$harmony.wnn_res0.4_clusters)
+
+model_19_cells_purple <- lmer(purple~Status+(1|individual),
+                       data = subset(purple_model_data, cluster ==19))
+summary(model_19_cells_purple)
+car::Anova(model_19_cells_purple, type = 'III')
+
+purple_pairs <- 
+as.data.frame(pairs(emmeans(model_19_cells_purple, 'Status'), p.adjust='none'))
+
+purple_pairs_filtered <- subset(purple_pairs, contrast %in% c('D - M',
+                                                              'D - F',
+                                                              'F - M'))
+#nothin
 
 
 
