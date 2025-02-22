5ebe1af59d5c3cf0a11bda3563cec900a54e0828
ggraham4 <ggraham4@illinois.edu>
2025-02-21 23:02:40 -0500

    Nearing the end of fig 2

--- DEG Analyses/Proportion DEGs/Prop DEGs SCTransform 022025.R
+++ DEG Analyses/Proportion DEGs/Prop DEGs SCTransform 022025.R
@@ -36,6 +36,7 @@ prop_cluster_plot<- readRDS( 'Functions/prop_cluster_plot.rds')
 define_degs_prop<- readRDS('Functions/define_degs_prop.rds')
 mean_expression_cluster_data<- readRDS('Functions/mean_expression_cluster_data.rds')
 prop_deg_function_sctransform<- readRDS('Functions/DEG_functions/prop_deg_function_sctransform.rds')
+clown_go<- readRDS('Functions/clown_go')
 
 }
 
@@ -75,12 +76,12 @@ for(i in c(0:31)){
 
 
 together_data_summed <- together_data%>%
-    subset(!is.na(class)& is.na(warning))%>%
+    subset(!is.na(class)& is.na(warning) & singular==F & prop_expressing_D >0.05 & prop_expressing_F>0.05 & prop_expressing_M>0.05)%>%
   group_by(cluster, class)%>%
   summarize(class_count = n())
 
 together_data_chisq <-  together_data%>%
-  subset(!is.na(class)& is.na(warning))%>%
+    subset(!is.na(class)& is.na(warning) & singular==F & prop_expressing_D >0.05 & prop_expressing_F>0.05 & prop_expressing_M>0.05)%>%
   group_by(cluster)%>%
   summarize(class_count = n())
 
@@ -97,18 +98,91 @@ together_data_defined_summed_plot <- together_data_summed%>%
   right_join(together_data_chisq, by = 'cluster')
 
 
-ggplot(together_data_defined_summed_plot, aes(x = as.factor(cluster), y = class_count.x, fill = class)) +
+together_data_defined_summed_plot$colors <- NA
+together_data_defined_summed_plot$colors <- ifelse(together_data_defined_summed_plot$class == 'Early Upregulated', 'blue', NA)
+together_data_defined_summed_plot$colors <- ifelse(together_data_defined_summed_plot$class == 'Early Downregulated', 'red', together_data_defined_summed_plot$colors )
+together_data_defined_summed_plot$colors <- ifelse(together_data_defined_summed_plot$class == 'Late Downregulated', 'pink', together_data_defined_summed_plot$colors )
+together_data_defined_summed_plot$colors <- ifelse(together_data_defined_summed_plot$class == 'Late Upregulated', 'orange', together_data_defined_summed_plot$colors )
+together_data_defined_summed_plot$colors <- ifelse(together_data_defined_summed_plot$class == 'Progressively Upregulated', 'cyan', together_data_defined_summed_plot$colors )
+together_data_defined_summed_plot$colors <- ifelse(together_data_defined_summed_plot$class == 'Progressively Downregulated', 'hotpink', together_data_defined_summed_plot$colors )
+together_data_defined_summed_plot$colors <- ifelse(together_data_defined_summed_plot$class == 'Terminally Downregulated', 'maroon', together_data_defined_summed_plot$colors )
+together_data_defined_summed_plot$colors <- ifelse(together_data_defined_summed_plot$class == 'Terminally Upregulated', 'darkgreen', together_data_defined_summed_plot$colors )
+together_data_defined_summed_plot$colors <- ifelse(together_data_defined_summed_plot$class == 'Transiently Downregulated', 'yellow', together_data_defined_summed_plot$colors )
+together_data_defined_summed_plot$colors <- ifelse(together_data_defined_summed_plot$class == 'Transiently Upregulated', 'green', together_data_defined_summed_plot$colors )
+
+together_data_defined_summed_plot$class <- factor(together_data_defined_summed_plot$class, 
+                                                  levels = rev(c('Transiently Upregulated',
+                                                             'Transiently Downregulated',
+                                                             'Terminally Upregulated',
+                                                             "Terminally Downregulated",
+                                                             'Progressively Upregulated',
+                                                             'Progressively Downregulated',
+                                                             'Late Upregulated',
+                                                             'Late Downregulated',
+                                                             'Early Upregulated',
+                                                             'Early Downregulated')))
+
+
+dpg_bar_plot <- ggplot(together_data_defined_summed_plot, aes(x = as.factor(cluster), y = class_count.x, fill = class)) +
   geom_bar(stat = "identity", position = "dodge") +
-  labs(x = "Cluster", y = "Number of DEGs") +
+  labs(x = "Cluster", y = "Number of DPGs", fill = 'Expression Pattern') +
   geom_bar(position="stack", stat="identity")+
   theme(axis.text.x = element_text(angle = -45, vjust = 1, hjust=0))+
-  scale_y_continuous()+
-  scale_fill_manual(values = P40)+
-geom_text(aes(label = issignif, y = class_count.y), size = 10)
-
+geom_text(aes(label = issignif, y = class_count.y), size = 8)+
+    scale_fill_manual(values = unique(together_data_defined_summed_plot$colors))+
+    theme_minimal()+
+    theme(axis.text.x = element_text(size = 10, vjust =0.4, angle = -90),
+        axis.text.y = element_text(size = 12),
+        legend.text = element_text(size=8),
+        legend.title  = element_text(size=12),
+        legend.background  = element_rect(color = 'white'),
+        #legend.position = c(.7,.6),
+        legend.direction = 'vertical',
+        legend.title.position = 'top',
+        legend.position = 'none'
+        )+
+  ylim(0,130)
+dpg_bar_plot
 
 table(together_data$class)
 length(together_data$gene[!is.na(together_data$class)])
+
+ggsave(plot = dpg_bar_plot,
+       file = "dpg_bar_plot.svg",
+       device = "svg",
+       units = "in",
+       width = 4,
+       height = 2,
+       path = "Bachelors Thesis/Plots/Figure 2")
+
+
+together_data_subset <- together_data%>%
+    subset(!is.na(class)& is.na(warning) & singular==F & prop_expressing_D >0.05 & prop_expressing_F>0.05 & prop_expressing_M>0.05)
+
+clown_go(together_data_subset$gene[together_data_subset$class=='Late Downregulated'])%>%dotplot()
+clown_go(together_data_subset$gene[together_data_subset$class=='Early Upregulated'])%>%dotplot()
+clown_go(together_data_subset$gene[together_data_subset$class=='Transiently Upregulated'])%>%dotplot()
+
+go_early_up <- clown_go(together_data_subset$gene[together_data_subset$class=='Early Upregulated'])%>%clusterProfiler::dotplot()
+
+
+go_early_up_plot <-go_early_up+ 
+  #coord_flip()+
+  theme(axis.text.y = element_text(),
+        axis.text.x = element_text(angle = -45),
+        legend.position = 'none')+
+  geom_point(fill = '#7c5c6c', color = 'black')+
+  scale_size_continuous(range = c(1,3))
+go_early_up_plot
+
+ggsave(plot = go_early_up_plot,
+       file = "go_early_up_plot.svg",
+       device = "svg",
+       units = "in",
+       width = 4,
+       height = 2,
+       path = "Bachelors Thesis/Plots/Figure 2")
+
 
   
 
