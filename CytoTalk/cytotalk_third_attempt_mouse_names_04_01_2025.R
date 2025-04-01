2c22198040d74416ab27a394057ab79e55fef2b7
Gabriel Graham <ggraham4@illinois.edu>
2025-04-01 15:28:56 -0500

    I guess im not gonna get any pathways huh

--- CytoTalk/cytotalk_third_attempt_mouse_names_04_01_2025.R
+++ CytoTalk/cytotalk_third_attempt_mouse_names_04_01_2025.R
@@ -8,40 +8,7 @@ py_require('pcst_fast')
 pcst_fast = import('pcst_fast')
 
 #read in human named data
-human_named =readRDS("C:/Users/Gabe/Desktop/RNA_object_human_names.rds")
-
-#convert to SCE
-human_named_sce =as.SingleCellExperiment(human_named)
-
-#read into cytotalk
-lst_scrna <- CytoTalk::from_single_cell_experiment(human_named_sce)
-
-cells_27 = rownames(human_named@meta.data[human_named@meta.data$harmony.wnn_res0.4_clusters==27,])
-cells_14 = rownames(human_named@meta.data[human_named@meta.data$harmony.wnn_res0.4_clusters==14,])
-
-
-#run cytotalk, lets use 27 -> 14 as an example\
-matrix_27 = as.matrix(human_named@assays$RNA$data[,human_named@meta.data$harmony.wnn_res0.4_clusters==27])
-matrix_14 = as.matrix(human_named@assays$RNA$data[,human_named@meta.data$harmony.wnn_res0.4_clusters==14])
-
-rownames(matrix_27) <- rownames(human_named@assays$RNA$data[,human_named@meta.data$harmony.wnn_res0.4_clusters==27])
-rownames(matrix_14) <- rownames(human_named@assays$RNA$data[,human_named@meta.data$harmony.wnn_res0.4_clusters==14])
-
-#write.csv(matrix_27, "A:/CytoTalk/scRNAseq-data/scRNAseq_27_sparse.csv")
-#write.csv(matrix_14, "A:/CytoTalk/scRNAseq-data/scRNAseq_14_sparse.csv")
-
-dir_in <- 'A:/CytoTalk/scRNAseq-data'
-lst_scrna <- CytoTalk::read_matrix_folder(dir_in)
-table(lst_scrna$cell_types)
-
-type_a = '27_sparse'
-type_b = '14_sparse'
-
-results <- CytoTalk::run_cytotalk(lst_scrna, type_a, type_b,
-                                  dir_out = 'A:/CytoTalk/27_14',
-                                  cores = detectCores()-1)
-# holy FUCK this thing is slow
-#doesnt even work 
+mouse_named =readRDS("C:/Users/Gabe/Desktop/RNA object mouse names.rds")
 
 cytotalk_wrapper = function(seurat_object,
                             sender,
@@ -53,18 +20,18 @@ cytotalk_wrapper = function(seurat_object,
   pcst_fast = import('pcst_fast')
   
   suppressWarnings(dir.create(paste0('A:/CytoTalk/',sender,'_',receiver)))
-
+  
   sender_matrix = as.matrix(seurat_object@assays$RNA$data[,seurat_object@meta.data$harmony.wnn_res0.4_clusters==sender])
   rownames(sender_matrix) <- rownames(seurat_object@assays$RNA$data[,seurat_object@meta.data$harmony.wnn_res0.4_clusters==sender])
   write.csv(sender_matrix, paste0("A:/CytoTalk/scRNAseq-data/scRNAseq_",sender,".csv"))
-
+  
   receiver_matrix <- as.matrix(seurat_object@assays$RNA$data[,seurat_object@meta.data$harmony.wnn_res0.4_clusters==receiver])
   rownames(receiver_matrix) <- rownames(seurat_object@assays$RNA$data[,seurat_object@meta.data$harmony.wnn_res0.4_clusters==receiver])
   write.csv(receiver_matrix, paste0("A:/CytoTalk/scRNAseq-data/scRNAseq_",receiver,".csv"))
   
   dir_in <- 'A:/CytoTalk/scRNAseq-data'
   lst_scrna <- CytoTalk::read_matrix_folder(dir_in)
-
+  
   type_a = paste0(sender)
   type_b = paste0(receiver)
   
@@ -78,6 +45,9 @@ cytotalk_wrapper = function(seurat_object,
   return(results)
 }
 
-resutls_19_5 <- cytotalk_wrapper(human_named, 19, 5)
+results_19_5 <- cytotalk_wrapper(mouse_named, 19, 5)
 
-resutls_19_5$pathways
+results_19_5$pathways
+results_27_14 <- cytotalk_wrapper(mouse_named, 27, 14)
+results_27_14$pathways
+
