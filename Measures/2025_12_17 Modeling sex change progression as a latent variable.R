44dbe47fd8e36b8585da8cedbbf620f228702940
Gabriel Graham <ggraham4@illinois.edu>
2025-12-17 20:41:49 -0500

    updat

--- Measures/2025_12_17 Modeling sex change progression as a latent variable.R
+++ Measures/2025_12_17 Modeling sex change progression as a latent variable.R
@@ -2,9 +2,13 @@ library(tidyverse)
 library(mgcv)
 library(ggplot2)
 library(factoextra)
+library(patchwork)
 
 
 coalesced = read.csv('Measures/coalesced_data.csv')
+multiome_measures = read.csv('Measures/all_data.csv')
+
+
 
 ggplot(coalesced, aes(x = mass_final, y = 10^Log_11KT))+
   geom_point(aes(color = Phase))+
@@ -27,7 +31,7 @@ ggplot(coalesced, aes(x = mass_final, y = 10^Log_E2))+
 
 ggplot(coalesced, aes(x = mass_final, y = Log_E2))+
   geom_point(aes(color = Phase))+
-    geom_smooth(aes(color=Phase), method= 'gam')
+  geom_smooth(aes(color=Phase), method= 'gam')
 
 ggplot(coalesced, aes(x = mass_final, y = Log_11KT))+
   geom_point(aes(color = Phase))+
@@ -72,10 +76,10 @@ ggplot(coalesced, aes(x = Log_11KT, y = Percent_Testicular))+
 
 # pca 1
 vars1 = c('Percent_Testicular',
-         'Log_11KT',
-         #'Log_E2',
-         'mass_final'
-         )
+          'Log_11KT',
+          #'Log_E2',
+          'mass_final'
+)
 
 pc_data1 = coalesced[complete.cases(coalesced[,vars1]),]%>%
   subset(!Phase %in% c('S',
@@ -86,10 +90,10 @@ X1 <- scale(pc_data1[, vars1])
 pca1 <- prcomp(X1, center = TRUE, scale. = TRUE)
 fviz_pca_biplot(pca1, habillage = pc_data1$Phase)
 
-pc_data1$pc1 <- pca$x[,1]
-pc_data1$pc2 <- pca$x[,2] *-1
+pc_data1$pc1 <- pca1$x[,1]
+pc_data1$pc2 <- pca1$x[,2] *-1
 
-pc_data1$Phase= factor(pc_data$Phase, levels = c('S','EP','NM','M','D','E','NF','F'))
+pc_data1$Phase= factor(pc_data1$Phase, levels = c('S','EP','NM','M','D','E','NF','F'))
 ggplot(pc_data1, aes(x = Phase, y = pc1))+
   geom_boxplot()+
   geom_jitter()
@@ -107,9 +111,9 @@ z2_1 = pca1$x[,2]
 
 ###pca2
 vars2 = c(#'Percent_Testicular',
-          'Log_11KT',
-          'Log_E2',
-          'mass_final'
+  'Log_11KT',
+  'Log_E2',
+  'mass_final'
 )
 
 pc_data2 = coalesced[complete.cases(coalesced[,vars2]),]%>%
@@ -169,6 +173,35 @@ ggplot(pc_data3, aes(x = Phase, y = pc2))+
   geom_jitter()
 # yeah just pc1 again
 
+# parsimonious pc4
+vars4= c(#'Percent_Testicular',
+  'Log_11KT',
+  'Log_E2'
+)
+
+##kt + e2
+pc_data4 = coalesced[complete.cases(coalesced[,vars4]),]%>%
+  subset(!Phase %in% c('S',
+                       'EP',
+                       'IDK',
+                       'NM'))
+X4 <- scale(pc_data4[, vars4])
+pca4 <- prcomp(X4, center = TRUE, scale. = TRUE)
+fviz_pca_biplot(pca4, habillage = pc_data4$Phase)
+
+pc_data4$pc1 <- pca4$x[,1]
+pc_data4$pc2 <- pca4$x[,2] *-1
+
+pc_data4$Phase= factor(pc_data4$Phase, levels = c('S','EP','NM','M','D','E','NF','F'))
+ggplot(pc_data4, aes(x = Phase, y = pc1))+
+  geom_boxplot()+
+  geom_jitter()
+
+ggplot(pc_data4, aes(x = Phase, y = pc2))+
+  geom_boxplot()+
+  geom_jitter()
+
+
 
 #some more theory
 pc_data3$Z = pc_data3$pc1
@@ -216,5 +249,40 @@ ggplot(pc_data3, aes(x =Z, y = Percent_Testicular))+
   geom_line(aes(x = Z, y = testis_curve, color = 'NLS Line'), size =2, color ='blue')
 
 
+a=ggplot(pc_data3, aes(x =Z, y = Log_11KT))+
+  geom_point(aes(color=Phase))
+
+b=ggplot(pc_data3, aes(x =Z, y = Log_E2))+
+  geom_point(aes(color=Phase))
+
+pc_data3$kt_e2 = pc_data3$Log_11KT/pc_data3$Log_E2
+c=ggplot(pc_data3, aes(x =Z, y = kt_e2))+
+  geom_point(aes(color=Phase))+
+  labs(y = "LogKT / Log E2")
+
+d=ggplot(pc_data3, aes(x =Z, y = mass_final))+
+  geom_point(aes(color=Phase))+
+  labs(y = "Mass (g)")
+
+e=ggplot(pc_data3, aes(x =Z, y = Percent_Testicular))+
+  geom_point(aes(color=Phase))
+
+female_z = mean(pc_data3$Z[pc_data3$Phase=='F'])
+dom_z = mean(pc_data3$Z[pc_data3$Phase=='D'])
+
+f=ggplot(pc_data3, aes(x =Z, y = days))+
+  labs(y = "Days Since Pairing")+
+  geom_point(aes(color=Phase))#
+# geom_vline(xintercept  = female_z)
+
+dat_behavior = pc_data3%>%
+  right_join(multiome_measures, by = 'Log_11KT')
+
+g=ggplot(dat_behavior, aes(x =Z, y = Behaviors_Day_2))+
+  labs(y = "Parental Behaviors")+
+  geom_point(aes(color=Phase))#
+
+
+a+b+c+d+e+g
 
 
